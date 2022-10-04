4e64082931abf45d6a7f8b3eb83330c2934b38a3
F. Ozge Ozturk <ozozge@gmail.com>
2022-10-04 16:54:14 -0400

    Update conditionalformat.R

--- conditionalformat.R
+++ conditionalformat.R
@@ -5,12 +5,12 @@ library(openxlsx)
 wb <- createWorkbook()
 
 addWorksheet(wb, "mysheet")
-writeData(wb , sheet = "mysheet", dson)
+writeData(wb , sheet = "mysheet", dataframe)
 
 negStyle <- createStyle(fontColour = "#9C0006", bgFill = "#FFC7CE")
 conditionalFormatting(wb, "mysheet",
                       cols = 6,
-                      rows = 1:nrow(dson), 
+                      rows = 1:nrow(dataframe), 
                       type = "contains", 
                       rule = "Aired", style = negStyle
 )
